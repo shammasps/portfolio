3f14e98ebf5d354ab949453b0b602ebd9c5e3c94
shammasps@gmail.com <shammasps@gmail.com>
2023-02-12 21:29:22 +0530

    6commit

--- javascript.js
+++ javascript.js
@@ -1,18 +1,19 @@
-var isLoaded = false;
-function windowScroll(){  
-  var isvisible = isScrolledIntoView($("#allskills")[0]);
-var isDivVisible1 = isScrolledIntoView($("#primary")[0]);
-var isDivVisible2 = isScrolledIntoView($("#content")[0]);
+var isAleadyLoaded = false;
+// this function will fire on the time of scroll (every time)
+function windowScroll(){ 
 
-  if(isvisible== false && isDivVisible1 == false && !isDivVisible2 ){
-    isLoaded = false;
+  var skillDiv = document.getElementById("allskills");
+  var isvisible = isScrolledIntoView(skillDiv);
+
+  if(isvisible== false){
+    isAleadyLoaded = false;
   }
 
-  if(isvisible || isDivVisible1 || isDivVisible2){
-    if(isLoaded == false){
+  if(isvisible){
+    if(isAleadyLoaded == false){
       actanim();
-    }        
-    isLoaded = true;
+      isAleadyLoaded = true;
+    }
   }  else {
     $("#circle1").css("animation","");
     $("#circle2").css("animation","");
@@ -28,8 +29,16 @@ function isScrolledIntoView(elem)
 
     var elemTop = $(elem).offset().top;
     var elemBottom = elemTop + $(elem).height();
-
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    if(elemTop <= docViewBottom && elemTop >= docViewTop ){
+      return true;
+    }
+    if(elemBottom >= docViewTop && elemBottom <= docViewBottom ){
+      return true;
+    }
+    if(elemTop < docViewTop && elemBottom > docViewBottom ){
+      return true;
+    }
+    return false;
 }
 function actanim() {
   
@@ -40,49 +49,49 @@ function actanim() {
 
 
   let number1 = document.getElementById('number1');
-let counter1 =0;
-setInterval(() => {
+  let counter1 =0;
+  setInterval(() => {
   if (counter1 == 80) {
     
   } else {
     counter1 += 1 ;
     number1.innerHTML = counter1 + "%";
   }
-}, 25);
+  }, 25);
 
-let number2 = document.getElementById('number2');
-let counter2 =0;
+  let number2 = document.getElementById('number2');
+  let counter2 =0;
   setInterval(() => {
   if (counter2 == 70) {
     
-   } else {
-     counter2 += 1 ;
-     number2.innerHTML = counter2 + "%";
-   }
- }, 29);
+    } else {
+      counter2 += 1 ;
+      number2.innerHTML = counter2 + "%";
+    }
+  }, 29);
 
 
- let number3 = document.getElementById('number3');
-let counter3 =0;
-setInterval(() => {
+  let number3 = document.getElementById('number3');
+  let counter3 =0;
+  setInterval(() => {
   if (counter3 == 75) {
     
   } else {
     counter3 += 1 ;
     number3.innerHTML = counter3 + "%";
   }
-}, 28);
+  }, 28);
 
-let number4 = document.getElementById('number4');
-let counter4 =0;
-setInterval(() => {
+  let number4 = document.getElementById('number4');
+  let counter4 =0;
+  setInterval(() => {
   if (counter4 == 65) {
     
   } else {
     counter4 += 1 ;
     number4.innerHTML = counter4 + "%";
   }
-}, 30);
+  }, 30);
 
 }
 
